4c58b2a7c1d5fd8cd1ad9678b19322906bd1d3da
Gedi <gedi@ScootyPuffJr.wifi.gsu.edu>
2019-06-22 23:35:56 -0400

    started tagging

--- InstagramClone/Structures/Tag.swift
+++ InstagramClone/Structures/Tag.swift
@@ -2,13 +2,16 @@
 //  Tag.swift
 //  InstagramClone
 //
-//  Created by Ahmed Gedi on 6/5/19.
-//  Copyright © 2019 Gedi, Ahmed M. All rights reserved.
-//
+
 
 import Foundation
 
 
 class Tag {
-    init() {}
+    var tagElements : [TagElement]?
+    var tagOccurance : Int?
+    var lastUsed : NSDate?
+    var firstUsed : NSDate?
+    
+    init(link : String) {}
 }
---
+++ InstagramClone/Structures/TagElement.swift
@@ -0,0 +1,15 @@
+//
+//  TagElement.swift
+//  InstagramClone
+//
+
+import Foundation
+
+class TagElement {
+
+    
+    var link : String?
+    var numOfUsages : Int?
+    var lastUsed : NSDate?
+    var firstUsed : NSDate?
+}
