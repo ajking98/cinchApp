7a58b2a9345b8a94c08db981529435b8b2ce2926
Ahmed Gedi <cinch@Ahmeds-MBP.wifi.gsu.edu>
2019-06-23 21:38:31 -0400

    worked on profile page

--- ProfilePageViewController.swift
+++ ProfilePageViewController.swift
@@ -8,25 +8,45 @@
 import UIKit
 
 class ProfilePageViewController: UIViewController {
-
+    
+//    @IBOutlet weak var fullName: UILabel!
+//    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var viewContainer: UIView!
+    @IBOutlet weak var customSegmentedControl: UISegmentedControl!
+    var reactionView: UIView!
+    var gemView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        buildSegmentedFolderControl()
         profileImage.layer.cornerRadius = 30
         profileImage.clipsToBounds = true
-
+        
+        
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func switchViewAction(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            customSegmentedControl.tintColor = UIColor(red: 1/255.0, green: 209/255.0, blue: 151/255.0, alpha: 1.0)
+            viewContainer.bringSubviewToFront(reactionView)
+        case 1:
+            customSegmentedControl.tintColor = UIColor(red: 1/255.0, green: 151/255.0, blue: 209/255.0, alpha: 1.0)
+            viewContainer.bringSubviewToFront(gemView)
+        default:
+            break
+        }
     }
-    */
-
+    
+    func buildSegmentedFolderControl() {
+        reactionView = ReactionsViewController().view
+        gemView = GemsViewController().view
+        viewContainer.addSubview(gemView)
+        viewContainer.addSubview(reactionView)
+    }
+    
 }
