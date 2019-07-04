854e6ba58f2a4055c468b83b0ff6ebdf0f52554c
Ahmed Gedi <cinch@Ahmeds-MacBook-Pro.local>
2019-07-04 10:54:37 -0400

    style changes

--- InstagramClone/Classes/GemsViewController.swift
+++ InstagramClone/Classes/GemsViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class GemsViewController: UIViewController {
 
+    @IBOutlet weak var image1: UIImageView!
+    @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var image3: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        image1.layer.cornerRadius = 12.0
+        image1.clipsToBounds = true
+        image2.layer.cornerRadius = 12.0
+        image2.clipsToBounds = true
+        image3.layer.cornerRadius = 12.0
+        image3.clipsToBounds = true
         // Do any additional setup after loading the view.
     }
 
