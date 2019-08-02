fa680d96db1f0104acdc0b96536741256f4b43d6
Gedi <agedi3@mstudio08.local>
2019-08-02 16:15:17 -0400

    fixed navigation controller and bugs

--- InstagramClone/DiscoverFolder/SelectedTablePostCell.swift
+++ InstagramClone/DiscoverFolder/SelectedTablePostCell.swift
@@ -1,54 +1,43 @@
 //
-//  TableViewPostCell.swift
+//  SelectedTablePostCell.swift
 //  InstagramClone
 //
-//  Created by Ahmed Gedi on 7/23/19.
+//  Created by Gedi, Ahmed M on 8/2/19.
 //  Copyright © 2019 Gedi, Ahmed M. All rights reserved.
 //
 
 import UIKit
 
-class TableViewPostCell: UITableViewCell {
+class SelectedTablePostCell: UITableViewCell {
 
     @IBOutlet weak var postImage: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var viewLabel: UILabel!
-    @IBOutlet weak var menuOption: UIButton!
-    @IBOutlet weak var shareButton: UIButton!
     @IBOutlet weak var likeButton: UIButton!
     var buttonClicked = true
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        postImage.layer
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
-    @IBAction func optionMenu(_ sender: Any) {
-        print("More Options")
-    }
     @IBAction func shareAction(_ sender: Any) {
         print("Share")
     }
+    
     @IBAction func likeAction(_ sender: Any) {
         if buttonClicked {
             print("Liked")
             UIView.animate( withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: .curveEaseInOut, animations: {
-                    self.likeButton.setImage(UIImage.init(named: "icons8-heart-pressed"), for: UIControl.State.normal)
-                    self.likeButton.transform = CGAffineTransform(scaleX: 2, y: 2)
-                    Helper().vibrate(style: .heavy)
-                    self.buttonClicked = false
+                self.likeButton.setImage(UIImage.init(named: "icons8-heart-pressed"), for: UIControl.State.normal)
+                self.likeButton.transform = CGAffineTransform(scaleX: 2, y: 2)
+                Helper().vibrate(style: .heavy)
+                self.buttonClicked = false
             })
-            UIView.animate( withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: .curveEaseInOut, animations: {
+            UIView.animate( withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: .curveEaseInOut, animations: {
                 self.likeButton.transform = CGAffineTransform(scaleX: 1, y: 1)
             })
         } else {
+            print("Unliked")
             UIView.animate( withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.8, options: .curveEaseInOut, animations: {
                 self.likeButton.setImage(UIImage.init(named: "icons8-heart-unpressed"), for: UIControl.State.normal)
                 self.likeButton.transform = CGAffineTransform(scaleX: 1, y: 1)
@@ -56,6 +45,11 @@ class TableViewPostCell: UITableViewCell {
                 self.buttonClicked = true
             })
         }
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
     }
 
 }
--- InstagramClone/Profile/ProfileContainer.swift
+++ InstagramClone/Profile/ProfileContainer.swift
@@ -27,8 +27,11 @@ class ProfileContainer: UIViewController {
                                                name: NSNotification.Name(rawValue: "Toggle Side Menu"),
                                                object: nil)
         
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handleProfilePan))
         firstView.addGestureRecognizer(panGesture)
+        
+        let panMenuGesture = UIPanGestureRecognizer(target: self, action: #selector(handleMenuPan))
+        secondView.addGestureRecognizer(panMenuGesture)
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapOut))
         firstView.addGestureRecognizer(tapGesture)
@@ -49,38 +52,70 @@ class ProfileContainer: UIViewController {
         rightMenuSideConstraint.constant = 300
     }
     
-    @objc func handlePan(gesture: UIPanGestureRecognizer) {
+    @objc func handleProfilePan(gesture: UIPanGestureRecognizer) {
+        gesture.isEnabled = true
+        print(gesture.view!.center.x)
         if gesture.state == .began || gesture.state == .changed {
             let translation = gesture.translation(in: self.view)
-            print(gesture.view!.center.x)
-            print("First View \(self.firstView.center.x)" )
-            print("Second View \(self.secondView.center.x)" )
-            if(gesture.view!.center.x < 600) {
-                self.firstView.center = CGPoint(x: gesture.view!.center.x + translation.x,y: gesture.view!.center.y)
-                self.secondView.center = CGPoint(x: self.secondView.center.x + translation.x,y: self.firstView.center.y)
+            if(gesture.view!.center.x < 220) {
+                self.firstView.center = CGPoint(x: gesture.view!.center.x + translation.x, y: gesture.view!.center.y)
+                self.secondView.center = CGPoint(x: self.secondView.center.x + translation.x, y: self.firstView.center.y)
                 gesture.view!.center = self.firstView.center
             }else {
-                self.firstView.center = CGPoint(x: 206, y: gesture.view!.center.y)
+                UIView.animateKeyframes(withDuration: 0.5, delay: 0, options: [], animations: {
+                    self.firstView.center = CGPoint(x: 207, y: gesture.view!.center.y)
+                    gesture.isEnabled = false
+                }, completion: nil)
             }
             
             gesture.setTranslation(CGPoint(x: 0,y: 0), in: self.view)
         } else if gesture.state == .ended {
-            if gesture.view!.center.x < 50 {
-                UIView.animateKeyframes(withDuration: 0.4, delay: 0, options: [], animations: {
-//                    gesture.view!.center = CGPoint(x: -90, y: gesture.view!.center.y)
-                    self.secondView.center = CGPoint(x: 300, y: gesture.view!.center.y)
-                   self.rightMenuSideConstraint.constant = 0
-                    self.isExpanded = true
+            if secondView.center.x < 400 {
+                UIView.animateKeyframes(withDuration: 0.3, delay: 0, options: [], animations: {
+                    self.firstView.center = CGPoint(x: -180, y: gesture.view!.center.y)
+                    self.secondView.center = CGPoint(x: 220, y: gesture.view!.center.y)
                 }, completion: nil)
                 
             } else {
-                UIView.animateKeyframes(withDuration: 0.4, delay: 0, options: [], animations: {
-                    gesture.view!.center = CGPoint(x: 207, y: gesture.view!.center.y)
-                   self.secondView.center = CGPoint(x: 564, y: gesture.view!.center.y)
-                    self.rightMenuSideConstraint.constant = 300
-                    self.isExpanded = false
+                UIView.animateKeyframes(withDuration: 0.3, delay: 0, options: [], animations: {
+                    self.firstView.center = CGPoint(x: 207, y: gesture.view!.center.y)
+                    self.secondView.center = CGPoint(x: 580, y: gesture.view!.center.y)
                 }, completion: nil)
             }
         }
     }
+    
+    @objc func handleMenuPan(gesture: UIPanGestureRecognizer) {
+        gesture.isEnabled = true
+        print(gesture.view!.center.x)
+        if gesture.state == .began || gesture.state == .changed {
+            let translation = gesture.translation(in: self.view)
+            if(gesture.view!.center.x > 190) {
+                self.firstView.center = CGPoint(x: self.firstView.center.x + translation.x,y: gesture.view!.center.y)
+                self.secondView.center = CGPoint(x: gesture.view!.center.x + translation.x,y: self.firstView.center.y)
+                gesture.view!.center = self.secondView.center
+            }else {
+                UIView.animateKeyframes(withDuration: 0.5, delay: 0, options: [], animations: {
+                    self.secondView.center = CGPoint(x: 207, y: gesture.view!.center.y)
+                    gesture.isEnabled = false
+                }, completion: nil)
+            }
+            
+            gesture.setTranslation(CGPoint(x: 0,y: 0), in: self.view)
+        } else if gesture.state == .ended {
+            if firstView.center.x < 0 {
+                UIView.animateKeyframes(withDuration: 0.3, delay: 0, options: [], animations: {
+                    self.firstView.center = CGPoint(x: -207, y: gesture.view!.center.y)
+                    self.secondView.center = CGPoint(x: 207, y: gesture.view!.center.y)
+                }, completion: nil)
+                
+            } else {
+                UIView.animateKeyframes(withDuration: 0.3, delay: 0, options: [], animations: {
+                    self.firstView.center = CGPoint(x: 207, y: gesture.view!.center.y)
+                    self.secondView.center = CGPoint(x: 621, y: gesture.view!.center.y)
+                }, completion: nil)
+            }
+        }
+    }
+    
 }
