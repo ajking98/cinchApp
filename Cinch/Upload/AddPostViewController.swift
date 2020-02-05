5dfe984bb1e4a82601037d3902571a2f629b280d
Alsahlani <yalsahlani3@mstudio18.local>
2020-02-05 11:52:53 -0500

    working on upload page

--- Cinch/Upload/AddPostViewController.swift
+++ Cinch/Upload/AddPostViewController.swift
@@ -1,14 +1,19 @@
 //
 //  AddPostViewController.swift
-//  Cinch
-//
+/*
+    The screen that is presented when the user has selected the media file from the image controller
+    Handles tagging and posting
+ */
 //  Created by Alsahlani, Yassin K on 2/4/20.
-//  Copyright © 2020 Gedi, Ahmed M. All rights reserved.
 //
 
 import UIKit
 
 class AddPostViewController: UIViewController, UIGestureRecognizerDelegate {
+    
+    //data
+    var width: CGFloat = 0
+    var height: CGFloat = 0
     
     //Elements
     let tagField = UITextView(frame: CGRect.zero)
@@ -21,10 +26,14 @@ class AddPostViewController: UIViewController, UIGestureRecognizerDelegate {
         setup()
         setupNavigationController()
         setupTagField()
+        setupPostButton()
+        setupImageView()
     }
     
     func setup() {
         view.backgroundColor = .white
+        width = view.frame.width
+        height = view.frame.height
     }
     
     func setupNavigationController() {
@@ -49,12 +58,42 @@ class AddPostViewController: UIViewController, UIGestureRecognizerDelegate {
         
         //constraints
         tagField.translatesAutoresizingMaskIntoConstraints = false
-        tagField.topAnchor.constraint(equalTo: view.topAnchor, constant: 0.12 * view.frame.height).isActive = true
+        tagField.topAnchor.constraint(equalTo: view.topAnchor, constant: 0.12 * height).isActive = true
         tagField.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 18).isActive = true
-        tagField.heightAnchor.constraint(equalToConstant: view.frame.height * 0.084).isActive = true
-        tagField.widthAnchor.constraint(equalToConstant: view.frame.width * 0.62).isActive = true
+        tagField.heightAnchor.constraint(equalToConstant: height * 0.084).isActive = true
+        tagField.widthAnchor.constraint(equalToConstant: width * 0.62).isActive = true
         
         addHashTag()
+    }
+    
+    func setupPostButton() {
+        postButton.setTitle("Post", for: .normal)
+        postButton.titleLabel!.font = UIFont(name: "Avenir-Black", size: 23)
+        postButton.backgroundColor = .customRed
+        postButton.layer.cornerRadius = 3
+        
+        view.addSubview(postButton)
+        
+        //constraints
+        postButton.translatesAutoresizingMaskIntoConstraints = false
+        postButton.leftAnchor.constraint(equalTo: tagField.rightAnchor, constant: width * 0.08).isActive = true
+        postButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 0.12 * height).isActive = true
+        postButton.heightAnchor.constraint(equalToConstant: width * 0.19).isActive = true
+        postButton.widthAnchor.constraint(equalToConstant: width * 0.19).isActive = true
+    }
+    
+    func setupImageView() {
+        imageView.backgroundColor = .white
+        imageView.image = UIImage(named: "f1")
+        imageView.contentMode = .scaleAspectFit
+        view.addSubview(imageView)
+        
+        //constraints
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        imageView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 0.787 * height).isActive = true
     }
     
     func addHashTag(){
